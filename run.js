2eaa7015a15b0f5c43b38f76b2171339b9fc6753
unknown <br36889@LACGT01H01194.lac.nsroot.net>
2017-02-13 15:33:59 -0600

    fixing regex

--- run.js
+++ run.js
@@ -140,7 +140,7 @@ function sendInfoList(message)
 			attributes : ['code','name']
 	})
 		.then (function(info) {
-					var output = JSON.stringify(info).replace(/[^a-zA-Z0-9 :\n\r]/g, "");					
+					var output = JSON.stringify(info).replace(/[^\n\r\sa-zA-Z0-9:]/g, "");					
 					message.author.sendMessage(output);	
 		})
 };
